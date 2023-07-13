a5ab95659580d15ef71ccb1335aafa3e13dd2a01
Przemek Denkiewicz <przemyslaw.denkiewicz@starburstdata.com>
2023-07-13 14:11:22 +0200

    test

--- ibis/backends/base/sql/alchemy/__init__.py
+++ ibis/backends/base/sql/alchemy/__init__.py
@@ -471,7 +471,7 @@ class BaseAlchemyBackend(BaseSQLBackend):
             warnings.filterwarnings(
                 "ignore", message="Did not recognize type", category=sa.exc.SAWarning
             )
-            warnings.filterwarnings(
+            warnings.simplefilter(
                 "ignore", category=sa.exc.SAWarning
             )
             table = sa.Table(
